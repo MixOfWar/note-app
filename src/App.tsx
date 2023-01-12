f6cc412b5804afbfd87e3d0cd2d37328a0114046
Mix Peace <mrssilentwar@gmail.com>
2023-01-11 18:20:54 -0900

    Note List page complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { useMemo } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import { v4 as uuidV4 } from 'uuid'
-import NewNote from './NewNote'
 import useLocalStorage from './useLocalStorage'
+import NewNote from './NewNote'
+import NoteList from './NoteList'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export type Note = {
@@ -59,10 +60,19 @@ const App = () => {
 	return (
 		<Container className='my-4'>
 			<Routes>
-				<Route path='/' element={<h1>Hello</h1>} />
+				<Route
+					path='/'
+					element={<NoteList availableTags={tags} notes={notesWithTags} />}
+				/>
 				<Route
 					path='/new'
-					element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags} />}
+					element={
+						<NewNote
+							onSubmit={onCreateNote}
+							onAddTag={addTag}
+							availableTags={tags}
+						/>
+					}
 				/>
 				<Route path='/:id' element={<h1>item</h1>}>
 					<Route index element={<h1>Show</h1>} />
